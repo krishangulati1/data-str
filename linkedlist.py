d1a944a8ca670b30e189c3d1cb52a3a6b1f3f1f7
krishangulati1 <krishangulati1996@gmail.com>
2018-08-26 17:44:05 +0530

    Function append was added

--- linkedlist.py
+++ linkedlist.py
@@ -7,7 +7,23 @@ class LinkedList():
     def __init__(self, head = None):
         self.head = head
 
+    def append(self, value):
+        current = self.head
+
+        if self.head:
+            while current.next:
+                current = current.next
+            current.value = value
+        else:
+            self.head = value
 
 
+e1 = Node(1)
+e2 = Node(2)
+e3 = Node(3)
+
+ll = LinkedList(e1)
+ll.append(e2)
+ll.append(e3)
 
 
